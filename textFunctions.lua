7366cdbf791c2cf8feead0bfce7a90069d757a8c
EnchantedAlchemy <154118441+EnchantedAlchemy@users.noreply.github.com>
2024-06-19 13:17:27 -0500

    Update textFunctions.lua

--- textFunctions.lua
+++ textFunctions.lua
@@ -1,66 +1,71 @@
 --Functions for simple terminal interaction and user input
 
 local function clear()
-    term.clear()
-    term.setCursorPos(1,1)
+	term.clear()
+	term.setCursorPos(1,1)
 end
 
 local function resetComputer()
 
-    settings.clear()
-    settings.save(".settings")
-    for i,v in pairs(fs.list("")) do
-        if fs.isReadOnly(v) == false and v ~= "resetComputer" then
-            shell.run("delete " .. v)
-        end
-    end
-    shell.run("delete resetComputer")
-    os.reboot()
-    return
+	settings.clear()
+	settings.save(".settings")
+	for i,v in pairs(fs.list("")) do
+		if fs.isReadOnly(v) == false and v ~= "resetComputer" then
+		shell.run("delete " .. v)
+		end
+	end
+	shell.run("delete resetComputer")
+	os.reboot()
+	return
 
 end
 
+function round(x)
+	return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+end
+  
+
 local function prompt(text, ...)
 
-    while true do
+	while true do
 
-        term.setTextColor(colors.white)
-        print(text)
+		term.setTextColor(colors.white)
+		print(text)
 
-        for i,v in ipairs(arg) do
-            print("(" .. i .. ") " .. v)
-        end
+		for i,v in ipairs(arg) do
+			print("(" .. i .. ") " .. v)
+		end
 
-        term.setTextColor(colors.yellow)
-        input = read()
-        term.setTextColor(colors.white)
+		term.setTextColor(colors.yellow)
+		input = read()
+		term.setTextColor(colors.white)
 
-        if input == nil or input == "" then
-            clear()
-            printError("Enter a valid input.")
-        else
-            return input
-        end
+		if input == nil or input == "" then
+			clear()
+			printError("Enter a valid input.")
+		else
+			return input
+		end
 
-    end
+	end
 
 end
 
 local function choicePrompt(text, ...)
 
-    while true do
+	while true do
 
-        input = tonumber(prompt(text, table.unpack(arg)))
+		input = tonumber(prompt(text, table.unpack(arg)))
 
-        if input == nil or arg[input] == nil then
-            clear()
-            printError("Enter a valid input.")
-        else
-            return input
-        end
+		if input == nil or arg[input] == nil then
+			clear()
+			printError("Enter a valid input.")
+		else
+			return input
+		end
 
-    end
+	end
 
 end
 
-return { clear = clear, resetComputer = resetComputer, prompt = prompt, choicePrompt = choicePrompt }
+return { clear = clear, resetComputer = resetComputer, round = round, prompt = prompt, choicePrompt = choicePrompt }
